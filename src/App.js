ebcd78d27c90ebff2e4f19d91ff33031bd89fdd8
Codec01011 <lildanni006@gmail.com>
2025-04-08 12:48:39 -0600

    adding the correct files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1>This is my JEnkins App</h1>
+    <h1>Daniel Eno</h1>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders This is my Jenkins App text', () => {
+test('renders Daniel Eno', () => {
   render(<App />);
   const textElement = screen.getByText(/This is my Jenkins App/i);
   expect(textElement).toBeInTheDocument();
